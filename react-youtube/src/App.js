070dc251960d2af11b967b27ab4ec11035768d8c
Samantha <samareimorgan@gmail.com>
2019-02-07 19:54:56 -0700

    Add basic grid structure in App.js

--- react-youtube/src/App.js
+++ react-youtube/src/App.js
@@ -1,26 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Container, Row, Col } from "reactstrap";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Container>
+        <Row>
+          <Col>
+            <h2> Search Bar Goes Here </h2>
+          </Col>
+          
+        </Row>
+        <Row>
+          <Col md="9">
+            <h2> Video Details go here</h2>
+          </Col>
+          <Col md="3">
+            <h2> Video List goes here</h2>
+          </Col>
+        </Row>
+      </Container>
     );
   }
 }
